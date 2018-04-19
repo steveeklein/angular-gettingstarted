6282ebf4b32ea07d9f0d9d8ca734a98d5abc1474
GitHubSteve <GitHubSteve@gmail.com>
2018-04-18 19:05:09 -0500

    fixed product detail

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
-import {HttpClientModule} from '@angular/common/http' ;
+import {HttpClientModule} from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component' ;
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpaces,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
